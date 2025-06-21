ff5d7ef4ec82a1b9ae7526c39732e59d2dce1933
Sohomz <sohomd088@gmail.com>
2025-06-21 10:57:00 +0530

    2nd commit after deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // This is the crucial part!
+  base: "/Password-Generator/", // <--- Add this line, make sure it matches your repo name exactly
   plugins: [react()],
-})
+});
